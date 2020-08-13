c6034e3cf575c1745156e25af009186837819cdf
Michael <example@gmail.com>
2020-08-13 16:29:04 +0800

    fixing slider bug when screen size changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,13 @@
 window.addEventListener('DOMContentLoaded', function() {
 
+	window.addEventListener('resize', function () { // При изменении размеров экрана устройства
+		callSlider();								// слайдер переопределяет свои размеры
+	});
+	
   //Slider native JS
   // проработать slidesToScroll
-	const track = document.querySelector('.comments__slider-track'),
+	function callSlider() {
+		const track = document.querySelector('.comments__slider-track'),
 		slidesToShow = 1,
 		slidesToScroll = 1,
 		slide = document.querySelectorAll('.comments__slider-slide'),
@@ -11,115 +16,123 @@ window.addEventListener('DOMContentLoaded', function() {
 		next = document.querySelector('.nextArrow'),
 		activeSlide = document.querySelector('.comments__slider-slide.active');
 
-	let itemWidth,
-		offset = 0,
-		posX,
-		slideIndex = 1;
+		let itemWidth,
+			offset = 0,
+			posX,
+			slideIndex = 1;
 
-	if(window.matchMedia('(max-width: 767px)').matches) {
-		itemWidth = (+(window.getComputedStyle(container).width).slice(0, -2));
-		track.style.width = `${100 * slide.length}%`;
-	}else {
-		itemWidth = (+(window.getComputedStyle(container).width).slice(0, -2)) / 3;
-	}
-	const movePosition = itemWidth * slidesToScroll;
-
-	if(window.matchMedia('(max-width: 767px)').matches) {
-		slideIndex = 0;
-		slide[slideIndex].classList.add('active');
-		slide[slideIndex+1].classList.remove('active');
-	}
-
-	slide.forEach((slide) => {
-		slide.style.minWidth = `${itemWidth}px`;
-		slide.style.transform = `scale(${0.60066})`;
-		if(slide.classList.contains('active')) {
-			slide.style.transform = `scale(${1})`;
-		}
-	});
-
-	prev.addEventListener('click', () => {
-		adjustMediaPrevBtn();
-		console.log(offset, slideIndex);
-		track.style.transform = `translateX(${offset}px)`;
-		slide[slideIndex].style.transform = `scale(${1})`; //central frame scale=1 opacity=1
-
-		if(slideIndex <= slide.length-2) {
-			slide[slideIndex+1].style.transform = `scale(${0.60066})`; //autoScale for small frames
-		} else {
-			slide[0].style.transform = `scale(${0.60066})`;
-		}
-		removeActive();
-		addActiveSlide(slideIndex);
-
-	});
-
-    next.addEventListener('click', () => {
-
-		adjustMediaNextBtn();
-		console.log(offset, slideIndex);
-		track.style.transform = `translateX(${offset}px)`;
-		slide[slideIndex].style.transform = `scale(${1})`;
-
-		if(slideIndex > 0) {
-			slide[slideIndex-1].style.transform = `scale(${0.60066})`;
-		} else {
-			slide[slide.length-1].style.transform = `scale(${0.60066})`;
-		}
-
-		removeActive();
-		addActiveSlide(slideIndex);
-	});
-
-	function adjustMediaNextBtn () {
-		if (window.matchMedia('(max-width: 767px)').matches) {
-			if (offset <= (-(itemWidth) * (slide.length-1))) {
-				offset = 0;
-				slideIndex = 0;
-			} else {
-				offset -= +movePosition;
-				slideIndex += (1*slidesToScroll);
-			}
-		}else {
-			if (offset <= (-(itemWidth) * (slide.length-2))) {
-				offset = itemWidth*slidesToScroll;
-				slideIndex = 0;
-			} else {
-				offset -= +movePosition;
-				slideIndex += (1*slidesToScroll);
-			}
-		}
-	}
-
-	function adjustMediaPrevBtn () {
 		if(window.matchMedia('(max-width: 767px)').matches) {
-			if (offset >= 0) {
-				offset = (-(itemWidth) * (slide.length-1)); //так как при загрузке страницы центральный кадр не первый слайд в html
-				slideIndex = slide.length - 1;				//то офсет на движение track сдвигаем на 2 слайда
-			} else {
-				offset += +movePosition;
-				slideIndex -= (1*slidesToScroll);
-			}
+			itemWidth = (+(window.getComputedStyle(container).width).slice(0, -2));
+			track.style.width = `${100 * slide.length}%`;
+			slideIndex = 0;
+			slide[slideIndex].classList.add('active');
+			slide[slideIndex+1].classList.remove('active');
 		}else {
-			if (offset >= itemWidth) {
-				offset = (-(itemWidth) * (slide.length-2)); //так как при загрузке страницы центральный кадр не первый слайд в html
-				slideIndex = slide.length - 1;				//то офсет на движение track сдвигаем на 2 слайда
+			itemWidth = Math.round((+(window.getComputedStyle(container).width).slice(0, -2)) / 3);
+			track.style.width = `inherit`;
+			slideIndex = 1;
+			slide[slideIndex].classList.add('active');
+			slide[slideIndex-1].classList.remove('active');
+		}
+		const movePosition = itemWidth * slidesToScroll;
+
+		if(window.matchMedia('(max-width: 767px)').matches) {
+			
+		}
+
+		slide.forEach((slide) => {
+			slide.style.minWidth = `${itemWidth}px`;
+			slide.style.transform = `scale(${0.60066})`;
+			if(slide.classList.contains('active')) {
+				slide.style.transform = `scale(${1})`;
+			}
+		});
+
+		prev.addEventListener('click', () => {
+			adjustMediaPrevBtn();
+			console.log(offset, slideIndex, itemWidth);
+			track.style.transform = `translateX(${offset}px)`;
+			slide[slideIndex].style.transform = `scale(${1})`; //central frame scale=1 opacity=1
+
+			if(slideIndex <= slide.length-2) {
+				slide[slideIndex+1].style.transform = `scale(${0.60066})`; //autoScale for small frames
 			} else {
-				offset += +movePosition;
-				slideIndex -= (1*slidesToScroll);
+				slide[0].style.transform = `scale(${0.60066})`;
+			}
+			removeActive();
+			addActiveSlide(slideIndex);
+
+		});
+
+		next.addEventListener('click', () => {
+
+			adjustMediaNextBtn();
+			console.log(offset, slideIndex);
+			track.style.transform = `translateX(${offset}px)`;
+			slide[slideIndex].style.transform = `scale(${1})`;
+
+			if(slideIndex > 0) {
+				slide[slideIndex-1].style.transform = `scale(${0.60066})`;
+			} else {
+				slide[slide.length-1].style.transform = `scale(${0.60066})`;
+			}
+
+			removeActive();
+			addActiveSlide(slideIndex);
+		});
+
+		function adjustMediaNextBtn () {
+			if (window.matchMedia('(max-width: 767px)').matches) {
+				if (offset <= (-(itemWidth) * (slide.length-1))) {
+					offset = 0;
+					slideIndex = 0;
+				} else {
+					offset -= +movePosition;
+					slideIndex += (1*slidesToScroll);
+				}
+			}else {
+				if (offset <= (-(itemWidth) * (slide.length-2))) {
+					offset = itemWidth*slidesToScroll;
+					slideIndex = 0;
+				} else {
+					offset -= +movePosition;
+					slideIndex += (1*slidesToScroll);
+				}
 			}
 		}
-	}
+
+		function adjustMediaPrevBtn () {
+			if(window.matchMedia('(max-width: 767px)').matches) {
+				if (offset >= 0) {
+					offset = (-(itemWidth) * (slide.length-1)); //так как при загрузке страницы центральный кадр не первый слайд в html
+					slideIndex = slide.length - 1;				//то офсет на движение track сдвигаем на 2 слайда
+				} else {
+					offset += +movePosition;
+					slideIndex -= (1*slidesToScroll);
+				}
+			}else {
+				if (offset >= itemWidth) {
+					offset = (-(itemWidth) * (slide.length-2)); //так как при загрузке страницы центральный кадр не первый слайд в html
+					slideIndex = slide.length - 1;				//то офсет на движение track сдвигаем на 2 слайда
+				} else {
+					offset += +movePosition;
+					slideIndex -= (1*slidesToScroll);
+				}
+			}
+		}
 
 	
-	function addActiveSlide(index) {
-		slide[index].classList.add('active');
+		function addActiveSlide(index) {
+			slide[index].classList.add('active');
+		}
+		function removeActive() {
+			slide.forEach((slide) => {
+				slide.classList.remove('active');
+			});
+		}
 	}
-	function removeActive() {
-		slide.forEach((slide) => {
-			slide.classList.remove('active');
-		});
-	}
+
+	callSlider();
 
 	//form validation
 	let consForm = document.querySelector('.feed-form__input');
